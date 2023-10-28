45b3717ecf6403e45518f1603bae61bae5e06273
Diti2604 <ditiboyboy@gmail.com>
2023-10-28 20:02:34 +0200

    day 1 complete

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains: ['image.tmdb.org']
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
