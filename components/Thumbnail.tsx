50d41027fc8bad613764a38fe2784eb8a16d4c86
Diti2604 <ditiboyboy@gmail.com>
2023-10-28 20:36:02 +0200

    small change 1st day

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -9,7 +9,7 @@ interface Props{
 
 function Thumbnail({movie}:Props) {
   return (
-    <div className="relative h-28 min-w-[100px] cursor-pointer 
+    <div className="relative h-28 min-w-[180px] cursor-pointer 
     transition duration-200 ease-out md:h-36 md:min-w-[260px]
      md:hover:scale-105 " >
     <Image
